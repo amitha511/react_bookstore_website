6282f3d5332dc67ded4c8355cac80d1c6dd63b80
Maya Keren <101181197+MayaKeren1@users.noreply.github.com>
2023-03-06 13:54:24 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/', buyingRouter);
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
